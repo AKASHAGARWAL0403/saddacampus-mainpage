efcc24a17f81b086c93973772f7bfe318bfb883f
akash <akashagarwal0403@gmail.com>
2018-06-23 14:40:27 +0530

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+module.exports = {
+
+	entry: './src/app.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js'
+    },
+	module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            include: path.resolve(__dirname, 'src')
+        }, {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+		historyApiFallback: true,
+		inline: true,
+		port: 8080
+    }
+};
